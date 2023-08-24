e9f094a2684d96d1cbaf56189794558b6e5dc43d
shawn-dev-5790 <shawn.dev.5790@gmail.com>
2023-08-24 15:11:37 +0900

    feat: :sparkles: 넥스트 프로젝트 셋팅

--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./types";
-
-export const getStringSeqId = (id: number) => {
-  return String(id);
-};
-export const getRandomType = (arr: string[]) => {
-  return arr[Math.floor(Math.random() * arr.length)];
-};
