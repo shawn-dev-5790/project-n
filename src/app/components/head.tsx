b702c50e334dbb97dbd1daeaa1af3f848c5b206a
shawn-dev-5790 <shawn.dev.5790@gmail.com>
2023-09-04 14:04:56 +0900

    feat: :sparkles: add app header

--- src/app/components/head.tsx
+++ src/app/components/head.tsx
@@ -1,43 +1,87 @@
 'use client'
 
+import Link from 'next/link'
 import style from './head.module.scss'
-import { AppLogoImg, AppMenuSignOutImg } from './image'
+import { AppIcon } from './image'
+import { useState } from 'react'
 
 export default function AppHead() {
   return (
     <div className={style.wrap}>
-      <AppLogo />
-      <AppMenu />
+      <div className={style.logo}>
+        <button>
+          <AppIcon name={'menu'} size={[24, 24]} />
+        </button>
+        <Link href={'/main'}>
+          <h1>
+            <span>DTR</span>
+            <span>X</span>
+            <span>SiteName</span> _ Kr
+          </h1>
+        </Link>
+      </div>
+      <div>search bars</div>
+      <div className={style.menu}>
+        <strong className="ir_so">Head Menu</strong>
+        <ActiveStatement />
+        <Notification />
+        <Setting />
+        <button className={style.avatar_btn} onClick={() => alert('avatar')}>
+          <span className={style.avatar_circle}>s</span>
+        </button>
+      </div>
     </div>
   )
 }
 
-export function AppLogo() {
+function ActiveStatement() {
+  const SITE_STATE: { [k: number]: string } = { 0: '데모', 1: '구독', 2: '만료' }
+  const [state, setState] = useState(0)
+  const onSelectState = () => setState((prev) => (prev === 0 ? 1 : prev === 1 ? 2 : 0))
   return (
-    <div className={style.logo}>
-      <h1>
-        <AppLogoImg />
-      </h1>
-      <p>
-        <b>Exploring with Next.js 13</b>
-        <small>A Prototyping Playground</small>
-      </p>
-    </div>
+    <button className={style.head_btn} onClick={onSelectState}>
+      <AppIcon name={'active'} size={[24, 24]} />
+      <span className={style.active_label}>{SITE_STATE[state]}</span>
+    </button>
   )
 }
-
-export function AppMenu() {
-  const menus = ['account', 'settings', 'notifications', 'profile', 'logout']
+function Notification() {
+  const [count, setCount] = useState(0)
+  const onIncreaseCount = () => setCount((prev) => prev + 7)
   return (
-    <div className={style.menu}>
-      <strong className="ir_so">Head Menu</strong>
-      <ul>
-        {menus.map((menu) => (
-          <li key={menu}>
-            <button>{menu.substring(0, 2).toLocaleUpperCase()}</button>
-          </li>
-        ))}
-      </ul>
+    <button className={style.head_btn} onClick={onIncreaseCount}>
+      <AppIcon name={'bell'} size={[24, 24]} />
+      <span className={style.noti_label}>{count}</span>
+    </button>
+  )
+}
+function Setting() {
+  const [open, setOpen] = useState(false)
+  const onToggle = () => setOpen((prev) => !prev)
+  return (
+    <div className="pos_rel">
+      <button className={style.head_btn} onClick={onToggle}>
+        <AppIcon name={'setting'} size={[24, 24]} />
+      </button>
+      {open && (
+        <div className={style.setting_menu}>
+          <button>
+            <AppIcon name={'theme'} size={[24, 24]} />
+            <span>디자인: 기기 테마</span>
+            <AppIcon name={'arrow_right'} size={[16, 16]} />
+          </button>
+          <button>
+            <AppIcon name={'lang'} size={[24, 24]} />
+            <span>언어: 한국어</span>
+            <AppIcon name={'arrow_right'} size={[16, 16]} />
+          </button>
+          <button>
+            <AppIcon name={'globe'} size={[24, 24]} />
+            <span>위치 : 한국</span>
+            <AppIcon name={'arrow_right'} size={[16, 16]} />
+          </button>
+        </div>
+      )}
     </div>
   )
 }
--- src/app/components/image.tsx
+++ src/app/components/image.tsx
@@ -1,5 +1,14 @@
 import Image from 'next/image'
 
+import setting from '@/assets/images/ico_setting.svg'
+import bell from '@/assets/images/ico_bell.svg'
+import active from '@/assets/images/ico_active.svg'
+import menu from '@/assets/images/ico_menu.svg'
+import lang from '@/assets/images/ico_lang.svg'
+import globe from '@/assets/images/ico_globe.svg'
+import theme from '@/assets/images/ico_theme.svg'
+import arrow_right from '@/assets/images/ico_arrow_right.svg'
+
 export function AppLogoImg() {
   return <Image src={'https://dummyimage.com/240x80/ffd900/000000&text=H1LOGO'} alt="H1LOGO" width={120} height={40} />
 }
@@ -7,4 +16,20 @@ export function AppMenuSignOutImg() {
   return (
     <Image src={'https://dummyimage.com/80x80/ffd900/000000&text=sign out'} alt="sign-out" width={40} height={40} />
   )
+}
+
+export function AppIcon({ name = 'setting', size = [16, 16] }: { name: string; size: [number, number] }) {
+  const icons = [
+    { name: 'setting', src: setting },
+    { name: 'bell', src: bell },
+    { name: 'active', src: active },
+    { name: 'menu', src: menu },
+    { name: 'lang', src: lang },
+    { name: 'globe', src: globe },
+    { name: 'theme', src: theme },
+    { name: 'arrow_right', src: arrow_right },
+  ]
+  const [width, heigh] = size
+  const target = icons.find((icon) => icon.name === name) || icons[0]
+  return <Image src={target.src} alt={target.name} width={width} height={heigh} />
 }
